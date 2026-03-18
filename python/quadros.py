2c7e669b711a1f1703aadbc0f5c8767eebbe252a
lorriant <brearey88@yandex.ru>
2026-03-18 09:23:19 +0900

    10 на 10 квадраты

---
+++ python/quadros.py
@@ -0,0 +1,33 @@
+import turtle
+
+t = turtle.Turtle()
+
+for j in range(10):
+  for i in range(10):
+    for k in range(4):
+      t.forward(20)
+      t.right(90)
+    t.right(90)
+    t.forward(25)
+    t.left(90)
+  t.forward(25)
+  t.left(90)
+  t.forward(250)
+  t.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.done()
